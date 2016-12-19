56eeeb3397251735a593267d7a6de1ebd9602ce0
yuwenli <841843777@qq.com>
2016-12-19 21:00:13 +0800

    1st

---
+++ test.py
@@ -0,0 +1,8 @@
+a=list(open('haha','r',encoding='utf8').readlines())
+for i in a:
+    print(i)
+print(type(a[1]))
+b=[s[0:-2] for s in list(file.readlines())]
+c=[int(s[-2]) for s in a]
+print(b)
+print(c)
